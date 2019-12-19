2cc658f95b8dbe1bc5bce259d7b496dab571d91e
Richad Wysocki <RichardWysocki@gmail.com>
2019-12-19 08:39:07 -0500

    Add in DBContext Classes

---
+++ DataAccess/BootStrapperContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+
+namespace DataAccess
+{
+    class BootStrapperContext : DbContext
+    {
+        public BootStrapperContext(DbContextOptions<BootStrapperContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+        
+    }
+}
---
+++ DataAccess/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace DataAccess
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
